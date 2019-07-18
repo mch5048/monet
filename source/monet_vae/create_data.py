aa5d032b7e4c967373ca6f2e0d2c275e32c909c2
sahanayvaz <sahanayvaz@gmail.com>
2019-07-18 14:24:27 +0200

    creating a dataset with ground truth masks

--- source/monet_vae/create_data.py
+++ source/monet_vae/create_data.py
@@ -36,10 +36,6 @@ N = 5e4
 N_same = int(N / 3)
 N_mixed = int(N - N_same)
 
-images = []
-
-d = {'s': 0, 'e': 0, 't': 0}
-
 def random_selector():
     idx_s = int(elems * np.random.uniform())
     idx_e = int(elems * np.random.uniform())
@@ -72,32 +68,40 @@ def random_selector():
         # ellipse is on the front
         l_s[e_idx] = 0.0
 
-    l_s = np.repeat(l_s, 3, axis=2)
-    l_e = np.repeat(l_e, 3, axis=2)
+    rl_s = np.repeat(l_s, 3, axis=2)
+    rl_e = np.repeat(l_e, 3, axis=2)
 
     # random coloring
     c_s = np.random.uniform(size=3)
     c_e = np.random.uniform(size=3)
     c_b = np.random.uniform(size=3)
 
-    l_s = l_s * c_s
-    l_e = l_e * c_e
-    background = background * c_b
+    rl_s = rl_s * c_s
+    rl_e = rl_e * c_e
+    background_colored = background * c_b
 
     c = np.random.uniform()
     r = 't'
     # with p=0.10 only one square, p=0.10 only one ellipse
     zeros = np.zeros((64, 64, 1))
 
+    epsilon = 1e-10
+    log_zeros = np.log(zeros + epsilon)
+    log_background = np.log(background + epsilon)
+    log_square = np.log(l_s + epsilon)
+    log_ellipse = np.log(l_e + epsilon)
     if c < 0.10:
         r = 's'
-        imgs = l_s + background
+        imgs = rl_s + background_colored
+        masks = np.concatenate([log_backgroung, log_square, log_zeros])
     elif c < 0.20:
         r = 'e'
-        imgs = l_e + background
+        imgs = rl_e + background_colored
+        masks = np.concatenate([log_backgroung, log_zeros, log_ellipse])
     else:
-        imgs = l_s + l_e + background
-    return imgs, r
+        imgs = rl_s + rl_e + background_colored
+        masks = np.concatenate([log_backgroung, log_square, log_ellipse])
+    return imgs, masks, r
 
 '''
 def random_selector(select_from1, select_from2, lb='mixed'):
@@ -136,30 +140,56 @@ for i in range(N_mixed):
     labels.append(l)
 '''
 
+
+images = []
+masks = []
+
+d = {'s': 0, 'e': 0, 't': 0}
+
+
 for i in range(int(N)):
-    im, r = random_selector()
+    im, m, r = random_selector()
     images.append(im)
+    masks.append(m)
     d[r] += 1
 
 images = np.array(images) 
+masks = np.array(masks)
 
 images = images.astype(np.float32)
+masks = masks.astype(np.float32)
 print(images.shape)
+print(masks.shape)
 
-plt.imshow(images[20] * 255.0)
-plt.show()
+def plot(image, masks):
+    mask1 = np.exp(masks[:, :, 0])
+    mask2 = np.exp(masks[:, :, 1])
+    mask3 = np.exp(masks[:, :, 2])
+    plt.subfigure(4, 2, 1):
+    plt.imshow(mask1, cmap='gray')
+    plt.subfigure(4, 2, 2):
+    plt.imshow(mask1 * image * 255)
 
-plt.imshow(images[30000] * 255.0)
-plt.show()
+    plt.subfigure(4, 2, 3):
+    plt.imshow(mask2, cmap='gray')
+    plt.subfigure(4, 2, 4):
+    plt.imshow(mask2 * image * 255)
 
-plt.imshow(images[400] * 255.0)
-plt.show()
+    plt.subfigure(4, 2, 5):
+    plt.imshow(mask2, cmap='gray')
+    plt.subfigure(4, 2, 6):
+    plt.imshow(mask2 * image * 255)
 
-plt.imshow(images[1000] * 255.0)
-plt.show()
+    plt.subfigure(4, 2, 7):
+    plt.imshow(image * 255)
+    plt.show()
+
+plot(images[20] * 255.0)
+plot(images[30000] * 255.0)
+plot(images[400] * 255.0)
 
 print('saving...')
 print(d)
-save_path = 'data/monet_2object_colored_64x64_normalized.npz'
-np.savez_compressed(save_path, imgs=images)
+save_path = 'data/monet_2object_colored_64x64_normalized_w_masks.npz'
+np.savez_compressed(save_path, imgs=images, masks=masks)
 print('saved to {}'.format(save_path))
